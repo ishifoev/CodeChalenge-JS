b775ccaf70a4a34db4be4ed773e71abe7741bb0c
Ismoil Shifoev <34019391+ishifoev@users.noreply.github.com>
2022-02-21 20:01:37 -0800

    Day 31

---
+++ Round4/longestPeak.js
@@ -0,0 +1,29 @@
+//O(n) time | O(1) space
+function longestPeak(array) {
+  // Write your code here.
+	let longestPeakLength = 0;
+	let i = 1;
+	while(i < array.length - 1) {
+		let isPeak = array[i - 1] < array[i] && array[i + 1] < array[i]
+ 		
+		if(!isPeak) {
+			i++;
+			continue;
+    }
+		let leftIdx = i - 2;
+		
+		while(leftIdx >=0 && array[leftIdx] < array[leftIdx + 1])
+		leftIdx--;
+		
+		let rightIdx = i + 2;
+		
+		while(rightIdx < array.length && array[rightIdx] < array[rightIdx - 1])
+		rightIdx++;
+		
+		let currentPeakLength = rightIdx - leftIdx - 1;
+		
+		longestPeakLength = Math.max(longestPeakLength, currentPeakLength)
+		i = rightIdx
+	}
+	return longestPeakLength
+}
