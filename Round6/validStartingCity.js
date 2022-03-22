5ff018e5d7bfef7070983313e154d8b15690b297
Ismoil Shifoev <34019391+ishifoev@users.noreply.github.com>
2022-03-22 13:37:57 +0500

    Day 59

---
+++ Round6/validStartingCity.js
@@ -0,0 +1,18 @@
+function validStartingCity(distances, fuel, mpg) {
+  // Write your code here.
+	let numberOfCities = distances.length;
+	
+	for(let startCityIdx = 0; startCityIdx < numberOfCities; startCityIdx++) {
+		let milesRemaining = 0;
+		for(let currentCityIdx = startCityIdx; currentCityIdx <  startCityIdx + numberOfCities; currentCityIdx++) {
+			if(milesRemaining < 0) continue;
+			const currentCityIdxRotated = currentCityIdx % numberOfCities
+			
+			let fulelFromCurrentCity = fuel[currentCityIdx]
+			let distanceToNextCity = distances[currentCityIdx]
+			let milesRemaining += fulelFromCurrentCity * mpg - distanceToNextCity;
+		}
+		if(milesRemaining > 0) return startCityIdx
+	}
+  return -1;
+}
