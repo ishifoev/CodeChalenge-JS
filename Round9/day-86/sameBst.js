4851b8743947ff56457dbbe37d0600ae712a5df9
Ismoil Shifoev <34019391+ishifoev@users.noreply.github.com>
2022-04-18 08:18:46 +0500

    Day 86

---
+++ Round9/day-86/sameBst.js
@@ -0,0 +1,30 @@
+//O(n ^ 2) time | O(n ^ 2) space
+function sameBsts(arrayOne, arrayTwo) {
+  if(arrayOne.length !== arrayTwo.length) return false;
+	
+	if(arrayOne.length === 0 && arrayTwo.length === 0) return true;
+	
+	if(arrayOne[0] !== arrayTwo[0]) return false;
+	
+	const leftOne = getSmaller(arrayOne);
+	const leftTwo = getSmaller(arrayTwo);
+	const rightOne = getBiggerOrEqual(arrayOne); 
+	const rightTwo = getBiggerOrEqual(arrayTwo);
+	return sameBsts(leftOne, leftTwo) && sameBsts(rightOne, rightTwo);
+}
+function getSmaller(array) {
+	const smaller = [];
+	for(let i = 1; i < array.length;i++) {
+		if(array[i] < array[0]) smaller.push(array[i])
+	}
+	return smaller;
+}
+function getBiggerOrEqual(array) {
+	const biggerOrEqual = [];
+	for(let i = 1; i < array.length; i++) {
+		if(array[i] >= array[0]) biggerOrEqual.push(array[i])
+	}
+	return biggerOrEqual;
+}
+// Do not edit the line below.
+exports.sameBsts = sameBsts;
